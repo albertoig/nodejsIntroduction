998f63b77a08f4549f321bbbbf9f624a8207d313
Alberto  Iglesias Gallego <alberto.uchiha@gmail.com>
2015-10-22 17:03:27 +0200

    Added new POST GET practice and some refactors

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 "use strict";
 
 let http = require('http');
+let config = require('./config.json');
 let server = http.createServer();
 
 let control = function (petic, resp) {
     resp.writeHead(200, {'content-type': 'text/plain'});
-    resp.write('\nHola, Mundo!\n by Alberto Iglesias Gallego\n\n');
+    resp.write('\nHola, Mundo!\n ' + config.author + '\n\n');
     resp.end();
 };
 
 server.on('request', control);
-server.listen(8080);
+server.listen(config.port);
